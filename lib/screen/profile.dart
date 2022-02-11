0376c46eb74655a43530a053727ad390fcf889d0
Nikunj Ramani <nikunjramani7624@gmail.com>
2022-02-11 17:40:03 +0530

    resolve pendind issues

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -18,41 +18,52 @@ class _ProfileState extends State<Profile> {
 
   @override
   void initState() {
-    // TODO: implement initState
-    super.initState();
     getUserProfile();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: employee != null
-            ? Column(
-                children: [
-                  buildUserInfoDisplay(employee.employeeName, "Name"),
-                  buildUserInfoDisplay(employee.email, "Email"),
-                  buildUserInfoDisplay(employee.dateOfBirth, "DOB"),
-                  buildUserInfoDisplay(employee.startDate, "Start Date"),
-                  buildUserInfoDisplay(employee.dateOfHire, "DOH"),
-                  buildUserInfoDisplay(employee.socialInsuranceNumber,
-                      "Social Insurance Number"),
-                  buildUserInfoDisplay(
-                      employee.employeeContactForm, "Employee Contact Form"),
-                  buildUserInfoDisplay(
-                      employee.employeeCourseName, "Employee Course Name"),
-                  buildUserInfoDisplay(
-                      employee.employeeCourseDate, "Employee Course Date"),
-                  buildUserInfoDisplay(employee.employeeCertificateOfCourse,
-                      "Employee Certificate Of   Course"),
-                  buildUserInfoDisplay(employee.diciplinaryActionNote,
-                      "Disciplinary Action Note"),
-                  buildUserInfoDisplay(
-                      employee.diciplinaryForm, "Disciplinary Form"),
-                  buildUserInfoDisplay(employee.assetOnHand, "Asset On Hand"),
-                ],
-              )
-            : Container(),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange[400],
+        title: Text("Profile"),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: employee != null
+                ? Column(
+                    children: [
+                      buildUserInfoDisplay(employee.employeeName ?? '', "Name"),
+                      buildUserInfoDisplay(employee.email ?? '', "Email"),
+                      buildUserInfoDisplay(employee.dateOfBirth ?? '', "DOB"),
+                      buildUserInfoDisplay(
+                          employee.startDate ?? '', "Start Date"),
+                      buildUserInfoDisplay(employee.dateOfHire ?? '', "DOH"),
+                      buildUserInfoDisplay(employee.socialInsuranceNumber ?? '',
+                          "Social Insurance Number"),
+                      buildUserInfoDisplay(employee.employeeContactForm ?? '',
+                          "Employee Contact Form"),
+                      buildUserInfoDisplay(employee.employeeCourseName ?? '',
+                          "Employee Course Name"),
+                      buildUserInfoDisplay(employee.employeeCourseDate ?? '',
+                          "Employee Course Date"),
+                      buildUserInfoDisplay(
+                          employee.employeeCertificateOfCourse ?? '',
+                          "Employee Certificate Of   Course"),
+                      buildUserInfoDisplay(employee.diciplinaryActionNote ?? '',
+                          "Disciplinary Action Note"),
+                      buildUserInfoDisplay(
+                          employee.diciplinaryForm ?? '', "Disciplinary Form"),
+                      buildUserInfoDisplay(
+                          employee.assetOnHand ?? '', "Asset On Hand"),
+                    ],
+                  )
+                : Container(),
+          ),
+        ),
       ),
     );
   }
@@ -66,22 +77,25 @@ class _ProfileState extends State<Profile> {
             title,
             style: const TextStyle(
               fontSize: 15,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+              color: Colors.black54,
             ),
           ),
           const SizedBox(
             height: 1,
           ),
           Container(
+              padding: EdgeInsets.only(left: 10),
+              alignment: Alignment.centerLeft,
               width: MediaQuery.of(context).size.width,
               height: 40,
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                color: Colors.grey,
-                width: 1,
-              ))),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(100, 211, 211, 211),
+                border: Border.all(
+                  color: const Color.fromARGB(100, 211, 211, 211),
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
               child: Text(
                 getValue,
                 style: const TextStyle(fontSize: 16, height: 1.4),
@@ -96,8 +110,9 @@ class _ProfileState extends State<Profile> {
     });
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
+    var data = await jsonDecode(response.body);
     setState(() {
-      employee = Employee.fromJson(jsonDecode(response.body));
+      employee = Employee.fromJson(data['data']);
     });
   }
 }
